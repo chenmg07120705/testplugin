7f701ef19d237e08e1fe8667c490792390b0aeb2
gdmec07120705 <756817242@qq.com>
2014-05-14 11:09:38 +0800

    07120705

---
+++ platforms/android/.staging/www/plugins/cn.edu.gdmec.t00385.helloworld/www/hello_world.js
@@ -0,0 +1,8 @@
+cordova.define("cn.edu.gdmec.t00385.helloworld.helloworld", function(require, exports, module) {var HelloWorld = function() {};  
+  
+HelloWorld.prototype.say = function() {  
+    alert("Hello World");  
+};  
+  
+var helloWorld = new HelloWorld();  
+module.exports = helloWorld;  });
